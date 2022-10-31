bbc770633f70abbfc196ce1eddbb8f0b03d0445b
Cristian David <98545319+CristianDCM@users.noreply.github.com>
2022-10-30 20:21:14 -0600

    Actualizo
    No he podido aun hacerle crecer, f

--- Snake.py
+++ Snake.py
@@ -1,91 +1,75 @@
 import random
-from pyfiglet import Figlet
-# Librery = pip install pyfiglet==0.7
-Font = Figlet(font='slant') 
+Snake = "■"
+Apple = "★"
 
-def main(): 
-    print("="*50)
-    print(Font.renderText('Culebrita'))
-    print("="*50)
-    tablero()
-    comida(tablero)
-    culebrita = "■"
-    # lista_culebrita = [] 
-    # longitud_culebrita = 1
+def Main(): 
+    Crear_tablero()
+    Pos_Snake_Random = Snake_Random(Snake) 
+    Pos_Apple = Comida(Apple)
+    Pos_Snake_Actual = []
+    Pos_Snake_Anterior = []
+
     while True:
-        mover_culebrita(culebrita, comida, tablero) 
+        Pos_Snake_Actual, Pos_Snake_Anterior = Mov(Pos_Snake_Random, Snake)
+        Crecer(Pos_Snake_Anterior)
+        Imprimir_tablero()
+        print(Pos_Snake_Actual, Pos_Snake_Anterior)
 
-        # cabeza_culebrita = [posicion_actual[0], posicion_actual[1]]
-        # lista_culebrita.append(cabeza_culebrita) 
+        if Pos_Snake_Actual == Pos_Apple:
+            Pos_Apple = Comida(Apple)
 
-        # if len(lista_culebrita) > longitud_culebrita:
-        #     del lista_culebrita[0] 
-
-        # for i in range(len(lista_culebrita)): 
-        #     tablero[lista_culebrita[i][0]][lista_culebrita[i][1]] = culebrita 
-
-        if buscar_comida(posicion_actual, posicion_comida) == True: 
-            comida(tablero) 
-            # longitud_culebrita += 1 
-            # print(lista_culebrita) 
-            # print(longitud_culebrita) 
+def Crecer(Pos_Snake_Anterior):
+    Tablero[Pos_Snake_Anterior[0]][Pos_Snake_Anterior[1]] = " "
         
-        for i in range(10): 
-            print(" ".join(tablero[i]))
-        print("="*50) 
-        print("")
+def Snake_Random(Snake):
+    Pos_Snake_Actual = [random.randint(0, 9), random.randint(0, 9)]
+    Tablero[Pos_Snake_Actual[0]][Pos_Snake_Actual[1]] = Snake
+    return Pos_Snake_Actual
 
-def tablero():
-    global tablero
-    tablero = [] 
+def Comida(Apple):
+    Pos_Apple = [random.randint(0, 9), random.randint(0, 9)]
+    Tablero[Pos_Apple[0]][Pos_Apple[1]] = Apple
+    return Pos_Apple
+
+def Mov(Pos_Snake_Actual, Snake):
+    Press = input("¿Dónde quieres mover la culebrita? (W, A, S, D): ")
+    Pos_Snake_Anterior = Pos_Snake_Actual[:]
+    if Press == "w" or Press == "W":
+        if Pos_Snake_Actual[0] == 0:
+            print("No puedes salirte del Tablero")
+        else:
+            Pos_Snake_Actual[0] -= 1
+    elif Press == "a" or Press == "A":
+        if Pos_Snake_Actual[1] == 0:
+            print("No puedes salirte del Tablero")
+        else:
+            Pos_Snake_Actual[1] -= 1
+    elif Press == "s" or Press == "S":
+        if Pos_Snake_Actual[0] == 9:
+            print("No puedes salirte del Tablero")
+        else:   
+            Pos_Snake_Actual[0] += 1
+    elif Press == "d" or Press == "D":
+        if Pos_Snake_Actual[1] == 9:
+            print("No puedes salirte del Tablero")
+        else:
+            Pos_Snake_Actual[1] += 1
+
+    Tablero[Pos_Snake_Actual[0]][Pos_Snake_Actual[1]] = Snake
+
+    return Pos_Snake_Actual, Pos_Snake_Anterior
+
+def Imprimir_tablero():
+    print("#" + " ─" * 10 + " #")
     for i in range(10):
-        tablero.append(["O"]*10)
+        print("|", " ".join(Tablero[i]), "|")
+    print("#" + " ─" * 10 + " #")
 
-def buscar_comida(posicion_actual, posicion_comida): 
-    if posicion_actual == posicion_comida:
-        return True
+def Crear_tablero():
+    global Tablero
+    Tablero = []
+    for i in range(10):
+        Tablero.append([" "]*10)
 
-def comida(tablero):
-    global posicion_comida 
-    comida = ("✩")
-    posicion_comida = [random.randint(0, 9), random.randint(0, 9)] 
-    tablero[posicion_comida[0]][posicion_comida[1]] = comida
-    
-def mover_culebrita(culebrita, comida, tablero):
-    global posicion_actual 
-    mover_culebrita = input("¿Dónde quieres mover la culebrita? (W, A, S, D): ")
-    posicion_actual = [0, 0] 
-    for i in range(10): 
-        for j in range(10): 
-            if tablero[i][j] == culebrita: 
-                posicion_actual = [i, j] 
-
-    if mover_culebrita == "w" or mover_culebrita == "W": 
-        if posicion_actual[0] == 0: 
-            print("No puedes salirte del tablero") 
-        else: 
-            tablero[posicion_actual[0]][posicion_actual[1]] = "O" 
-            tablero[posicion_actual[0]-1][posicion_actual[1]] = culebrita 
-
-    elif mover_culebrita == "a" or mover_culebrita == "A":
-        if posicion_actual[1] == 0:
-            print("No puedes salirte del tablero")
-        else:
-            tablero[posicion_actual[0]][posicion_actual[1]] = "O"
-            tablero[posicion_actual[0]][posicion_actual[1]-1] = culebrita
-
-    elif mover_culebrita == "s" or mover_culebrita == "S":
-        if posicion_actual[0] == 9:
-            print("No puedes salirte del tablero")
-        else:
-            tablero[posicion_actual[0]][posicion_actual[1]] = "O"
-            tablero[posicion_actual[0]+1][posicion_actual[1]] = culebrita
-
-    elif mover_culebrita == "d" or mover_culebrita == "D":
-        if posicion_actual[1] == 9:
-            print("No puedes salirte del tablero")
-        else:
-            tablero[posicion_actual[0]][posicion_actual[1]] = "O"
-            tablero[posicion_actual[0]][posicion_actual[1]+1] = culebrita
-
-main()
+if __name__ == "__main__":
+    Main()
