4d50f3b70e64644914ff54fd3e4afcba68ee535e
Cristian David <cristianzdmc14@gmail.com>
2022-11-04 14:17:59 -0500

    Actualizando...

--- Snake.py
+++ Snake.py
@@ -1,27 +1,18 @@
 import random
-def Main(): 
-    Crear_tablero()
-    Pos_Snake_Random = Snake_Random() 
-    Pos_Apple = Comida()
-    Pos_Snake_Actual = []
 
-    while True:
-        Pos_Snake_Actual= Mov(Pos_Snake_Random)
-        Imprimir_tablero()
-        if Pos_Snake_Actual == Pos_Apple:
-            Pos_Apple = Comida()
+def Crear_tablero():
+    global Tablero
+    Tablero = []
+    for i in range(H):
+        Tablero.append([" "] * W)
 
-def Snake_Random():
-    Pos_Snake_Actual = [random.randint(0, 9), random.randint(0, 9)]
-    Tablero[Pos_Snake_Actual[0]][Pos_Snake_Actual[1]] = "■"
-    return Pos_Snake_Actual
+def Imprimir_tablero():
+    print("#" + " ─" * H + " #")
+    for i in range(10):
+        print("|", " ".join(Tablero[i]), "|")
+    print("#" + " ─" * H + " #")
 
-def Comida():
-    Pos_Apple = [random.randint(0, 9), random.randint(0, 9)]
-    Tablero[Pos_Apple[0]][Pos_Apple[1]] = "★"
-    return Pos_Apple
-
-def Mov(Pos_Snake_Actual):
+def Mov():
     Press = input("¿Dónde quieres mover la culebrita? (W, A, S, D): ")
     if Press == "w" or Press == "W":
         if Pos_Snake_Actual[0] == 0:
@@ -44,20 +35,22 @@ def Mov(Pos_Snake_Actual):
         else:
             Pos_Snake_Actual[1] += 1
     Tablero[Pos_Snake_Actual[0]][Pos_Snake_Actual[1]] = "■"
-    return Pos_Snake_Actual
 
-def Imprimir_tablero():
-    print("#" + " ─" * H + " #")
-    for i in range(10):
-        print("|", " ".join(Tablero[i]), "|")
-    print("#" + " ─" * H + " #")
+def Comida():
+    Pos_Apple = [random.randint(0, 9), random.randint(0, 9)]
+    Tablero[Pos_Apple[0]][Pos_Apple[1]] = "★"
+    return Pos_Apple
 
-def Crear_tablero():
-    global Tablero
-    Tablero = []
-    for i in range(H):
-        Tablero.append([" "] * W)
+def Main(): 
+    Crear_tablero()
+    Pos_Apple = Comida()
+    while True:
+        Imprimir_tablero()
+        Mov()
+        if Pos_Snake_Actual == Pos_Apple:
+            Pos_Apple = Comida()
 
+Pos_Snake_Actual = [random.randint(0, 9), random.randint(0, 9)]
 W = 10
 H = 10
 
