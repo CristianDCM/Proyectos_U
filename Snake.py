502732868d260c822878ba0c64d9e31e7e367349
Cristian David <cristianzdmc14@gmail.com>
2022-11-04 14:35:44 -0500

    Actualizando..

--- Snake.py
+++ Snake.py
@@ -7,6 +7,12 @@ def Crear_tablero():
         Tablero.append([" "] * W)
 
 def Imprimir_tablero():
+    for i in Cuerpo_Snake:
+        if i == Cuerpo_Snake[0]:
+            Tablero[i[0]][i[1]] = "■"
+        else:
+            Tablero[i[0]][i[1]] = "▪"
+
     print("#" + " ─" * H + " #")
     for i in range(10):
         print("|", " ".join(Tablero[i]), "|")
@@ -15,26 +21,13 @@ def Imprimir_tablero():
 def Mov():
     Press = input("¿Dónde quieres mover la culebrita? (W, A, S, D): ")
     if Press == "w" or Press == "W":
-        if Pos_Snake_Actual[0] == 0:
-            print("No puedes salirte del Tablero")
-        else:
-            Pos_Snake_Actual[0] -= 1
+        Cuerpo_Snake.insert(0, [Cuerpo_Snake[0][0] - 1, Cuerpo_Snake[0][1]])
     elif Press == "a" or Press == "A":
-        if Pos_Snake_Actual[1] == 0:
-            print("No puedes salirte del Tablero")
-        else:
-            Pos_Snake_Actual[1] -= 1
+        Cuerpo_Snake.insert(0, [Cuerpo_Snake[0][0], Cuerpo_Snake[0][1] - 1])
     elif Press == "s" or Press == "S":
-        if Pos_Snake_Actual[0] == 9:
-            print("No puedes salirte del Tablero")
-        else:   
-            Pos_Snake_Actual[0] += 1
+        Cuerpo_Snake.insert(0, [Cuerpo_Snake[0][0] + 1, Cuerpo_Snake[0][1]])
     elif Press == "d" or Press == "D":
-        if Pos_Snake_Actual[1] == 9:
-            print("No puedes salirte del Tablero")
-        else:
-            Pos_Snake_Actual[1] += 1
-    Tablero[Pos_Snake_Actual[0]][Pos_Snake_Actual[1]] = "■"
+        Cuerpo_Snake.insert(0, [Cuerpo_Snake[0][0], Cuerpo_Snake[0][1] + 1])
 
 def Comida():
     Pos_Apple = [random.randint(0, 9), random.randint(0, 9)]
@@ -47,10 +40,10 @@ def Main():
     while True:
         Imprimir_tablero()
         Mov()
-        if Pos_Snake_Actual == Pos_Apple:
+        if Cuerpo_Snake[0] == Pos_Apple:
             Pos_Apple = Comida()
 
-Pos_Snake_Actual = [random.randint(0, 9), random.randint(0, 9)]
+Cuerpo_Snake = [[1,1], [1,2], [1,3]]
 W = 10
 H = 10
 
