c2cb6ae387db7d2e1833be062a75fe4e1e749d63
Cristian David <cristianzdmc14@gmail.com>
2022-11-04 14:20:08 -0500

    Comment

--- Snake_Reference.py
+++ Snake_Reference.py
@@ -1,83 +1,77 @@
-import os
 import random
 
-def Crecer_Snake():
-    global Comida
-    Nueva_Cabeza = Cuerpo_Snake[0][0] + \
-        Direction[0], Cuerpo_Snake[0][1] + Direction[1]
-    Cuerpo_Snake.insert(0, Nueva_Cabeza)
-    if not Comida:
-        Cuerpo_Snake.pop(-1)
-    Comida = False
+def Crecer_Snake():  #Funcion para que el cuerpo de la serpiente crezca
+    global Comida #Variable global para que la comida se agregue al cuerpo de la serpiente
+    Nueva_Cabeza = Cuerpo_Snake[0][0] + Direction[0], Cuerpo_Snake[0][1] + Direction[1] #Nueva cabeza de la serpiente
+    Cuerpo_Snake.insert(0, Nueva_Cabeza) #Inserta la nueva cabeza de la serpiente
+    if not Comida:  #Si no hay comida, elimina el ultimo elemento de la serpiente #AQUI ES
+        Cuerpo_Snake.pop(-1) #Elimina el ultimo elemento de la serpiente
+    Comida = False #La comida se vuelve falsa para que la serpiente no crezca
 
-def Mov():
-    global Direction
-    Controls = {"a": (-1, 0), "d": (1, 0), "w": (0, -1), "s": (0, 1)}
-    Press = input("Presiona una tecla: (W, A ,S ,D) ")
-    if Press == "w":
-        Direction = Controls["w"]
+def Mov():  #Funcion para mover la serpiente
+    global Direction #Variable global para que la direccion de la serpiente se pueda cambiar
+    Controls = {"a": (-1, 0), "d": (1, 0), "w": (0, -1), "s": (0, 1)} #Controles para mover la serpiente
+    Press = input("Presiona una tecla: (W, A ,S ,D) ") #Pide al usuario que presione una tecla
+    if Press == "w":    #Si presiona w, la serpiente se mueve hacia arriba
+        Direction = Controls["w"] #La direccion de la serpiente es hacia arriba
     elif Press == "s":
         Direction = Controls["s"]
     elif Press == "a":
         Direction = Controls["a"]
     elif Press == "d":
         Direction = Controls["d"]
-    elif Press == "q":
-        exit()
 
-def Colision_comida():
-    global Comida, Posicion_comida
-    if Cuerpo_Snake[0] == Posicion_comida:
-        Posicion_comida = Colocar_comida()
-        Comida = True
+def Colision_comida():  #Funcion para que la serpiente coma la comida
+    global Comida, Posicion_comida #Variable global para que la comida se agregue al cuerpo de la serpiente
+    if Cuerpo_Snake[0] == Posicion_comida: #Si la cabeza de la serpiente esta en la posicion de la comida
+        Posicion_comida = Colocar_comida() #La posicion de la comida se vuelve la nueva posicion de la comida
+        
 
-def Colocar_comida():
-    Col = random.randint(1, W-2)
-    Row = random.randint(1, H-2)
-    while (Col, Row) in Cuerpo_Snake:
-        Col = random.randint(1, W-2)
-        Row = random.randint(1, H-2)
-    return (Col, Row)
+def Colocar_comida(): #Funcion para colocar la comida
+    Col = random.randint(1, W-2) #Colocacion de la comida en el eje x
+    Row = random.randint(1, H-2) #Colocacion de la comida en el eje y
+    while (Col, Row) in Cuerpo_Snake: #Si la comida esta en el cuerpo de la serpiente
+        Col = random.randint(1, W-2)    #La comida se vuelve a colocar en el eje x
+        Row = random.randint(1, H-2)   #La comida se vuelve a colocar en el eje y
+    return (Col, Row) #Retorna la posicion de la comida
 
-def Imprimir_tablero():
-    for i in Tablero:
-        if i in Cuerpo_Snake:
-            print("■", end="")
-        elif i == Posicion_comida:
-            print("X", end="")
-        elif i[1] in (0, H-1) or i[0] in (0, W-1):
-            print("#", end="")
-        else:
-            print(" ", end="")
-        if i[0] == W-1:
-            print("")
+def Imprimir_tablero(): #Funcion para imprimir el tablero
+    for i in Tablero: #Para cada elemento en el tablero
+        if i in Cuerpo_Snake: #Si el elemento esta en el cuerpo de la serpiente
+            print("■", end="") #Imprime un ■
+        elif i == Posicion_comida: #Si el elemento esta en la posicion de la comida
+            print("X", end="") #Imprime una X
+        elif i[1] in (0, H-1) or i[0] in (0, W-1): #Si el elemento esta en el borde del tablero
+            print("#", end="") #Imprime un #
+        else: #Si no esta en el cuerpo de la serpiente, en la posicion de la comida o en el borde del tablero
+            print(" ", end="") #Imprime un espacio
+        if i[0] == W-1: #Si el elemento esta en el borde del tablero
+            print("") #Imprime un salto de linea
 
-def Game_Over():
-    if Cuerpo_Snake[0][0] in (0, W-1) or Cuerpo_Snake[0][1] in (0, H-1):
-        print("Game Over")
-        exit()
-    for i in Cuerpo_Snake[1:]:
-        if Cuerpo_Snake[0] == i:
-            print("Game Over")
-            exit()
+def Game_Over(): #Funcion para que el juego termine
+    if Cuerpo_Snake[0][0] in (0, W-1) or Cuerpo_Snake[0][1] in (0, H-1): #Si la cabeza de la serpiente esta en el borde del tablero
+        print("Game Over") #Imprime Game Over
+        exit() #Termina el juego
+    for i in Cuerpo_Snake[1:]: #Para cada elemento en el cuerpo de la serpiente
+        if Cuerpo_Snake[0] == i: #Si la cabeza de la serpiente esta en el cuerpo de la serpiente
+            print("Game Over") #Imprime Game Over
+            exit() #Termina el juego
+ 
+def Main(): #Funcion principal
+    while True: #Ciclo infinito
+        Imprimir_tablero() #Imprime el tablero
+        Mov() #Mueve la serpiente
+        Crecer_Snake() #Hace que el cuerpo de la serpiente crezca
+        print(Posicion_comida) #Imprime la posicion de la comida
 
-global W, H
-W = 25
-H = 15
-Cuerpo_Snake = [(4, H//2), (3, H//2), (2, H//2)]
-Tablero = [(Columna, Fila) for Fila in range(H) for Columna in range(W)]
-Controls = {"a": (-1, 0), "d": (1, 0), "w": (0, -1), "s": (0, 1)}
-Posicion_comida = Colocar_comida()
-Comida = False
+global W, H #Variables globales para el ancho y el alto del tablero
+W = 25 #Ancho del tablero
+H = 15 #Alto del tablero
+Cuerpo_Snake = [(4, H//2), (3, H//2), (2, H//2)] #Cuerpo de la serpiente con una longitud de 3 elementos
+Tablero = [(Columna, Fila) for Fila in range(H) for Columna in range(W)] #Tablero
 
-def Main():
-    print(Tablero)
-    while True:
-        Imprimir_tablero()
-        Mov()
-        Crecer_Snake()
-        Colision_comida()
-        Game_Over()
+Posicion_comida = Colocar_comida()  #Posicion de la comida
+Comida = False #Comida falsa para que la serpiente no crezca
 
-if __name__ == "__main__":
-    Main()
+if __name__ == "__main__": #Si el archivo se esta ejecutando
+    Main() #Ejecuta la funcion principal
