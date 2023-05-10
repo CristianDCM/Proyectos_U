2d3f46d3114252d06d0cbb0ff46e4f35d6f42a77
Cristian David <cristianzdmc14@gmail.com>
2023-05-09 21:19:02 -0500

    Movimiento continuo

--- Snake.py
+++ Snake.py
@@ -2,90 +2,90 @@ import random
 from pytimedinput import timedInput # pip install pytimedinput
 import os # pip install os-sys
 
-def Crear_tablero():
-    global Tablero
-    Tablero = []
+def creartablero():
+    global tablero
+    tablero = []
     for i in range(H):
-        Tablero.append([" "] * W)
+        tablero.append([" "] * W)
 
-def Imprimir_tablero():
+def imprimirTablero():
     for i in range(H): #Recorre las filas
         for j in range(W): #Recorre las columnas
-            if [i, j] in Cuerpo_Snake: #Si la posición está en la lista del cuerpo de la culebrita
-                if [i, j] == Cuerpo_Snake[0]: #Si la posición es la cabeza de la culebrita
-                    Tablero[i][j] = "◉" #Pone la cabeza de la culebrita
+            if [i, j] in cuerpoSnake: #Si la posición está en la lista del cuerpo de snake
+                if [i, j] == cuerpoSnake[0]: #Si la posición es la cabeza de snake
+                    tablero[i][j] = "◉" #Pone la cabeza de snake
                 else:
-                    Tablero[i][j] = "●"
+                    tablero[i][j] = "●"
             else:
-                if Tablero[i][j] != "★":
-                    Tablero[i][j] = " "
+                if tablero[i][j] != "★":
+                    tablero[i][j] = " "
 
     print("╔" + "══" * W + "═╗") # Borde superior
-    for i in Tablero:
+    for i in tablero:
         print("║", end=" ") # Borde izquierdo
         for j in i:
             print(j, end=" ") # Cuerpo del tablero
         print("║") # Borde derecho
     print("╚" + "══" * W + "═╝") # Borde inferior
 
-def Crecimiento_Mov():
-    Nueva_Cabeza = [Cuerpo_Snake[0][0] + Movimiento_Snake[0], Cuerpo_Snake[0][1] + Movimiento_Snake[1]]
-    Cuerpo_Snake.insert(0, Nueva_Cabeza)
-    if Cuerpo_Snake[0] != Pos_Apple:
-        Cuerpo_Snake.pop()
+def crecimientoMov():
+    nuevaCabeza = [cuerpoSnake[0][0] + movimientoSnake[0], cuerpoSnake[0][1] + movimientoSnake[1]]
+    cuerpoSnake.insert(0, nuevaCabeza)
+    if cuerpoSnake[0] != posicApple:
+        cuerpoSnake.pop()
 
 def Game_Over():
-    if Cuerpo_Snake[0][0] == -1 or Cuerpo_Snake[0][0] == H or Cuerpo_Snake[0][1] == -1 or Cuerpo_Snake[0][1] == W: # Si la cabeza de la culebrita toca los bordes
+    if cuerpoSnake[0][0] == -1 or cuerpoSnake[0][0] == H or cuerpoSnake[0][1] == -1 or cuerpoSnake[0][1] == W: # Si la cabeza de la culebrita toca los bordes
         return True 
-    for i in Cuerpo_Snake[1:]: # Si la cabeza de la culebrita toca su cuerpo 
-        if Cuerpo_Snake[0] == i:
+    for i in cuerpoSnake[1:]: # Si la cabeza de la culebrita toca su cuerpo 
+        if cuerpoSnake[0] == i:
             return True
 
-def Controles():
-    global Movimiento_Snake
-    Controles = {"W": [-1, 0], "A": [0, -1], "S": [1, 0], "D": [0, 1]}  # Arriba, Izquierda, Abajo, Derecha
-    Press, _ = timedInput("¿Dónde quieres mover la culebrita? (W, A, S, D): ", timeout=0.3) # Introduce una tecla y si no introduce nada, se mueve hacia la misma dirección
-    if Press == "w" or Press == "W":
-        Movimiento_Snake = Controles["W"]
-    elif Press == "a" or Press == "A":
-        Movimiento_Snake = Controles["A"]
-    elif Press == "s" or Press == "S":
-        Movimiento_Snake = Controles["S"]
-    elif Press == "d" or Press == "D":
-        Movimiento_Snake = Controles["D"]
+def controles():
+    global movimientoSnake
+    controles = {"W": [-1, 0], "A": [0, -1], "S": [1, 0], "D": [0, 1]}  # Arriba, Izquierda, Abajo, Derecha
+    press, _ = timedInput("¿Dónde quieres mover la culebrita? (W, A, S, D): ", timeout=0.3) # Introduce una tecla y si no introduce nada, se mueve hacia la misma dirección
+    if press == "w" or press == "W":
+        movimientoSnake = controles["W"]
+    elif press == "a" or press == "A":
+        movimientoSnake = controles["A"]
+    elif press == "s" or press == "S":
+        movimientoSnake = controles["S"]
+    elif press == "d" or press == "D":
+        movimientoSnake = controles["D"]
     else:
-        if Press == "":
+        if press == "":
             pass
         else:
             print("Introduce una tecla válida")
 
-def Posicion_Comida():
-    global Pos_Apple
-    Pos_Apple = [random.randint(0, H - 1), random.randint(0, W - 1)]
-    while Pos_Apple in Cuerpo_Snake:
-        Pos_Apple = [random.randint(0, H - 1), random.randint(0, W - 1)]
-    Tablero[Pos_Apple[0]][Pos_Apple[1]] = "★"
+def posiccomida():
+    global posicApple
+    posicApple = [random.randint(0, H - 1), random.randint(0, W - 1)]
+    while posicApple in cuerpoSnake:
+        posicApple = [random.randint(0, H - 1), random.randint(0, W - 1)]
+    tablero[posicApple[0]][posicApple[1]] = "★"
 
-def Main():
-    Crear_tablero()
-    Posicion_Comida()
+def main():
+    creartablero()
+    posiccomida()
     while True:
         os.system("cls") # Limpia la pantalla
-        Imprimir_tablero()
-        Controles()
-        Crecimiento_Mov()
+        imprimirTablero()
+        controles()
+        crecimientoMov()
         if Game_Over():
             print("Game Over")
             break
-        if Cuerpo_Snake[0] == Pos_Apple:
-            Posicion_Comida()
-        print("Score: ", len(Cuerpo_Snake) - 2)
+        if cuerpoSnake[0] == posicApple:
+            posiccomida()
+        print("Score: ", len(cuerpoSnake) - 2)
 
 H = 10  # Alto
 W = 10  # Ancho
-Cuerpo_Snake = [[1, 5], [1, 6], [1, 7]]  # Cuerpo Snake(Cabeza, Cuerpo, Cola)
-Comida = False
-Movimiento_Snake=[1, 0] 
+cuerpoSnake = [[1, 5], [1, 6], [1, 7]]  # Cuerpo Snake(Cabeza, Cuerpo, Cola)
+comida = False
+movimientoSnake=[1, 0] 
 
 if __name__ == "__main__":
-    Main()
+    main()
