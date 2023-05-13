c2146831adeed5cd65644ed07288b3989ff76d63
Cristian David <cristianzdmc14@gmail.com>
2023-05-12 23:43:43 -0500

    Succes DB

--- Connection.py
+++ Connection.py
@@ -1,10 +1,25 @@
-import cx_Oracle as cx
+import mysql.connector
 
-def get_connection():
-    user , password  = 'system' , '12345'  
-    try :
-        con = cx.connect(user,password,'localhost:1521/xe')
-        print("Conexión exitosa")
-        return con
-    except cx.Error as error:
-        print("Error al conectar a la base de datos ",error)
+def connect():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            port = 3307,
+            user = "root",
+            password = "usbw",
+            database = "gamesnake"
+        )
+    except mysql.connector.Error as e:
+        print("Error en la conexión", e)
+        connection = None
+    return connection
+
+def insert(connection, nickname, score, time, fecha):
+    try:
+        cursor = connection.cursor()
+        sql = "INSERT INTO pointstable (nickname, score, time, datetime) VALUES (%s, %s, %s, %s)"
+        val = (nickname, score, time, fecha)
+        cursor.execute(sql, val)
+        connection.commit()
+    except mysql.connector.Error as e:
+        print("Error al insertar datos", e)
