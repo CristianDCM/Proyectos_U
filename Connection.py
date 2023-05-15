5c0dba8d61552638701e41c8cd496865fa617d36
Cristian David <cristianzdmc14@gmail.com>
2023-05-14 22:59:53 -0500

    sqlaclchemy?

--- Connection.py
+++ Connection.py
@@ -1,25 +1,19 @@
-import mysql.connector
+import sqlalchemy as db
+
 
 def connect():
     try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            port = 3307,
-            user = "root",
-            password = "usbw",
-            database = "gamesnake"
-        )
-    except mysql.connector.Error as e:
-        print("Error en la conexión", e)
-        connection = None
-    return connection
+        engine = db.create_engine('mysql+mysqlconnector://root:usbw@localhost:3307/gamesnake')
+        connection = engine.connect()
+        return connection
+    except db.exc.SQLAlchemyError as e:
+        print("Error al conectar con la base de datos", e)
+    
 
 def insert(connection, nickname, score, time, fecha):
     try:
-        cursor = connection.cursor()
-        sql = "INSERT INTO pointstable (nickname, score, time, datetime) VALUES (%s, %s, %s, %s)"
-        val = (nickname, score, time, fecha)
-        cursor.execute(sql, val)
-        connection.commit()
-    except mysql.connector.Error as e:
-        print("Error al insertar datos", e)
+        query = db.insert('pointstable').values(nickname=nickname, score=score, time=time, fecha=fecha)
+        ResultProxy = connection.execute(query)
+        return ResultProxy
+    except Exception as e:
+        print("Error: ", e)
