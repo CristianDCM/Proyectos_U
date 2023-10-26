11c1f269178493ceda83f3a5f18b249b82dc294b
CDCM <cristianzdmc14@gmail.com>
2023-10-25 20:14:47 -0500

    Update Connection.py

--- Connection.py
+++ Connection.py
@@ -1,19 +1,13 @@
-import sqlalchemy as db
-
+import mysql.connector
 
 def connect():
     try:
-        engine = db.create_engine('mysql+mysqlconnector://root:usbw@localhost:3307/gamesnake')
-        connection = engine.connect()
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="gamesnake",
+    )
         return connection
-    except db.exc.SQLAlchemyError as e:
-        print("Error al conectar con la base de datos", e)
-    
-
-def insert(connection, nickname, score, time, fecha):
-    try:
-        query = db.insert('pointstable').values(nickname=nickname, score=score, time=time, fecha=fecha)
-        ResultProxy = connection.execute(query)
-        return ResultProxy
-    except Exception as e:
-        print("Error: ", e)
+    except mysql.connector.Error as err:
+        print(err)
